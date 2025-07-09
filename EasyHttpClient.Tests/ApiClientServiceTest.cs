8193c4682a8970073174b01fc78141dcd9b3b1c4
Pedro <pedro.silva@waydatasolution.com>
2025-07-09 20:57:39 -0300

    Aplicação de testes

--- EasyHttpClient.Tests/ApiClientServiceTest.cs
+++ EasyHttpClient.Tests/ApiClientServiceTest.cs
@@ -1,13 +1,55 @@
 ﻿using Moq;
+using System.Net;
+using System.Text;
+using EasyHttpClient;
 
-namespace EasyHttpClient.Tests
+public class ApiClientServiceTest
 {
-    public class ApiClientServiceTest
+    [Fact]
+    public async Task GetAsync_DeveRetornarListaVazia_QuandoJsonForInvalido()
     {
+        // Arrange
+        var jsonValido = """
+            [
+                { "id": "1" },
+                { "id": "2" }
+            ]
+            """;
+        var handler = new FakeHttpMessageHandler(jsonValido);
+        var httpClient = new HttpClient(handler);
 
-        [Fact]
-        public async Task GetAsync_DeveRetornarListaVazia_QuandoJsonForInvalido()
+        var httpClientFactory = new Mock<IHttpClientFactory>();
+        httpClientFactory.Setup(x => x.CreateClient(It.IsAny<string>())).Returns(httpClient);
+
+        var service = new ApiClientService<teste>(httpClientFactory.Object);
+
+        // Act
+        var resultado = await service.GetAsync("http://qualquerurl.com");
+
+        // Assert
+        Assert.NotNull(resultado);
+    }
+
+    public class FakeHttpMessageHandler : HttpMessageHandler
+    {
+        private readonly string _responseContent;
+
+        public FakeHttpMessageHandler(string responseContent)
         {
+            _responseContent = responseContent;
         }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(_responseContent, Encoding.UTF8, "application/json")
+            });
+        }
+    }
+
+    public class teste
+    {
+        public string Id { get; set; }
     }
 }
